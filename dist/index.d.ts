709bbbf50bf0111fab44a4605ff34f9ff36cb98c
br4e <br4e@protonmail.com>
2021-10-11 12:37:11 -0700

    Add in dist

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+import { BigNumber } from "ethers";
+export declare const enforce: (conditional: boolean, errorMessage: string) => void;
+export declare const firstOrNull: <T>(array: T[]) => T | null;
+export declare const first: <T>(array: T[]) => T;
+export declare const last: <T>(array: T[]) => T;
+export declare type PromiseType<T> = T extends PromiseLike<infer U> ? U : T;
+export declare const unscale: (quantity: BigNumber, decimals?: number) => number;
---
+++ dist/index.js
@@ -0,0 +1,44 @@
+// =================== GENERAL UTILS =====================
+export var enforce = function (conditional, errorMessage) {
+    if (!conditional)
+        throw new Error(errorMessage);
+};
+/// ================== FIRST / LAST IN LIST ======================
+export var firstOrNull = function (array) {
+    if (array.length === 0)
+        return null;
+    return array[0];
+};
+export var first = function (array) {
+    enforce(array.length > 0, 'First for empty array');
+    return array[0];
+};
+export var last = function (array) {
+    enforce(array.length > 0, 'Last for empty array');
+    return array[array.length - 1];
+};
+// type PromiseType = PromiseType<typeof promisedOne> // => number
+// ===================== MATH ===========================
+export var unscale = function (quantity, decimals) {
+    if (decimals === void 0) { decimals = 18; }
+    var digits = quantity.toString().length;
+    var digitsToRemove = digits - 15;
+    if (digitsToRemove > decimals) {
+        throw new Error('number too large');
+    }
+    while (digitsToRemove > 9) {
+        quantity = quantity.div(1e9);
+        digitsToRemove -= 9;
+        decimals -= 9;
+    }
+    var num = 0;
+    if (digitsToRemove > 0) {
+        decimals -= digitsToRemove;
+        num = quantity.div(Math.pow(10, digitsToRemove)).toNumber();
+    }
+    else {
+        num = quantity.toNumber();
+    }
+    var result = num / (Math.pow(10, decimals));
+    return result;
+};
